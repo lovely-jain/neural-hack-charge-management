e69671fd3316085de2249aba9b9ac6a0e4783564
Lovely Jain <68026750+lovely-jain@users.noreply.github.com>
2020-10-19 11:04:09 +0530

    Update pending.component.ts

--- charge-app/src/app/pending/pending.component.ts
+++ charge-app/src/app/pending/pending.component.ts
@@ -18,6 +18,8 @@ export class PendingComponent implements OnInit {
   ngOnInit(): void {
     if(sessionStorage.getItem('role')!="approver" && sessionStorage.getItem('role')!="creator" )
     {
+      
+      alert("Access Not Allowed");
       this._router.navigate(['/']);
     }
     else{
